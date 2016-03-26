19194fd1bc7f46971ff4a5a260d3b127cef2a4f6
Gayan Jayasundara <gayanjayasundara@bluedrop.com>
2016-03-25 21:49:33 -0230

    Initial Commit with Singal page project

---
+++ hustle/hustle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hustle
+//
+//  Created by Gayan Jayasundara on 2016-03-25.
+//  Copyright © 2016 Gayan Jayasundara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
